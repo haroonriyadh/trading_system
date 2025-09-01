a0fae954d60f719330142ad5047c01cf9efc46dc
haroonriyadh <108120204+haroonriyadh@users.noreply.github.com>
2025-09-01 11:26:29 +0300

    Update WebSocket_Real_time.py

--- WebSocket_Real_time.py
+++ WebSocket_Real_time.py
@@ -1,14 +1,19 @@
 from Database import db_candle,Redis
-from pybit.unified_trading import WebSocket
-from time import sleep
 from symbols import symbols
 import json
+import logging
+from datetime import datetime
+import websocket
 
-ws = WebSocket(
-    testnet=True,
-    channel_type="linear",
-)
-def handle_message(data):
+logging.basicConfig(filename='logfile_multi_kline.log', level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(message)s')
+
+interval = 1
+# بناء قائمة المواضيع (topics) للاشتراك
+topics = [f"kline.{interval}.{symbol}" for symbol in symbols]
+
+def on_message(ws,message):
+    data = json.loads(message)
     candle = data['data'][0]
     # استخراج اسم الرمز من topic
     symbol = data['topic'].split(".")[-1]
@@ -27,13 +32,47 @@ def handle_message(data):
     if candle["confirm"]:
         db_candle[symbol].insert_one(candle_obj)
 
-    print(f"{symbol}: {candle_obj}")
+    print(f"Time: {datetime.fromtimestamp(int(candle['start'])/1000)} ,Open: {candle['open']}, High: {candle['high']}, Low: {candle['low']}, Close: {candle['close']}, Volume: {candle['volume']}")
+    print("-" * 60)
+
+def on_error(ws, error):
+    print('⚠️ WebSocket Error:', error)
 
 
-ws.kline_stream(
-    interval=1,
-    symbol=symbols,
-    callback=handle_message
-)
-while True:
-    sleep(5)
+def on_close(ws, close_status_code, close_msg):
+    print("### WebSocket Closed ###", close_status_code, close_msg)
+
+
+def on_open(ws):
+    print('✅ WebSocket Opened')
+    sub_msg = {"op": "subscribe", "args": topics}
+    ws.send(json.dumps(sub_msg))
+    print(f"Subscribed to: {topics}")
+
+
+def on_pong(ws, *data):
+    print('pong received')
+
+
+def on_ping(ws, *data):
+    now = datetime.now()
+    print("ping received at", now.strftime("%H:%M:%S"))
+
+
+def connWS():
+    ws = websocket.WebSocketApp(
+        "wss://stream.bybit.com/v5/public/linear",   # Spot
+        # إذا أردت العقود (Linear): "wss://stream.bybit.com/v5/public/linear"
+        on_message=on_message,
+        on_error=on_error,
+        on_close=on_close,
+        on_ping=on_ping,
+        on_pong=on_pong,
+        on_open=on_open
+    )
+    ws.run_forever(ping_interval=20, ping_timeout=10)
+
+
+if __name__ == "__main__":
+    websocket.enableTrace(False)  # اجعلها True لو تريد تتبع كل الأحداث
+    connWS()
