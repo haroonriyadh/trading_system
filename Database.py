c470f6d10005ef21fa4376761f729eb02728d97b
haroonriyadh <haroon.riyadh2020@gmail.com>
2025-09-01 03:08:20 +0300

    Fix Database.py file to Turn Redis

--- Database.py
+++ Database.py
@@ -7,6 +7,6 @@ db_candle = mongo_client['CandleStick_data']
 db_OB = mongo_client['Order_Block']
 db_Orders = mongo_client['Open_Orders']
 
-Redis = redis.Redis(host='localhost', port=6379, db=0)
+Redis = redis.Redis(host='redis', port=6379, db=0)
 
 
