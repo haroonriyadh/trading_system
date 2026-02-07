b35891324e8d9ac7a09a5052eb24467aa3371c19
haroonriyadh <haroon@example.com>
2026-02-07 10:33:48 +0300

    Fix Execution engine

--- services/execution_engine/bybit_client.py
+++ services/execution_engine/bybit_client.py
@@ -16,7 +16,7 @@ symbols_info = {item["symbol"]: item for item in resp["result"]["list"] if item[
 
 # حفظ في ملف JSON
 with open("exchange_info.json", "w", encoding="utf-8") as f:
-    js.dump(symbols_info, f, indent=4, ensure_ascii=False)
+    json.dump(symbols_info, f, indent=4, ensure_ascii=False)
 
 print(f"تم حفظ البيانات في exchange_info.json ✅ for {len(symbols_info)} coin ")
 
