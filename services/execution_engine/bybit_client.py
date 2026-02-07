0d883ecd75a22ff7b16a5d70d8b8ad5a6e85e4b0
haroonriyadh <haroon@example.com>
2026-02-07 10:32:28 +0300

    Fix Execution engine

--- services/execution_engine/bybit_client.py
+++ services/execution_engine/bybit_client.py
@@ -4,13 +4,22 @@ from datetime import datetime
 import json
 import time
 import math
+import requests
 
 
-# =========================
-# Load Exchange_info
-# =========================
-with open('exchange_info.json') as f:
-    symbols_info = json.load(f)
+url = "https://api.bybit.com/v5/market/instruments-info"
+params = {"category": "linear","limit":"1000"}
+resp = requests.get(url, params=params).json()
+
+symbols_info = {item["symbol"]: item for item in resp["result"]["list"] if item["symbol"].endswith("USDT") and item["status"] == "Trading"}
+
+
+# حفظ في ملف JSON
+with open("exchange_info.json", "w", encoding="utf-8") as f:
+    js.dump(symbols_info, f, indent=4, ensure_ascii=False)
+
+print(f"تم حفظ البيانات في exchange_info.json ✅ for {len(symbols_info)} coin ")
+
 
 # =========================
 # Create session
